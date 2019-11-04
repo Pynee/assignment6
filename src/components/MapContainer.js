78a414d68e3649bac752c5527d0bf80e7a6d05a2
Pynee <jaakkoyl@hotmail.com>
2019-11-04 13:28:18 +0200

    Update to use enviroment variable for API key

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -4,9 +4,10 @@ import GoogleMapReact from "google-map-react";
 const Map = props => {
   return (
     // Important! Always set the container height explicitly
+
     <div className="flex-grow">
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyCT9N5ZwipknQQUyV30QkxGtzLl9Oe8J90" }}
+        bootstrapURLKeys={{ key: process.env.REACT_APP_API_KEY }}
         center={props.center}
         zoom={props.zoom}
         yesIWantToUseGoogleMapApiInternals
